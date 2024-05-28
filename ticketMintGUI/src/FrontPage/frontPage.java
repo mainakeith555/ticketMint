2d8ba0f15ad1ba9946af64f245e4189c8443c86e
mainakeith <mainakeith@gmail.com>
2024-05-28 15:44:13 -0400

    Added smart contacts folder

---
+++ ticketMintGUI/src/FrontPage/frontPage.java
@@ -0,0 +1,20 @@
+package FrontPage;
+import javax.swing.*;
+import java.io.*;
+
+
+public class frontPage extends JToggleButton{
+
+    private JFileChooser fileChooser;
+    private JToggleButton toggleButton1;
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello, World!");
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
